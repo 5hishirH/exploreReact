23cf5c73c790d55a714bce09b269af261c63b772
Shishir Halder <s.halder0108@gmail.com>
2023-09-08 12:44:58 +0600

    feat: way of styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,32 @@ function App() {
 
   return (
     <>
-      <h1>Greetings to React</h1>
+      <h1 style={Linkon()}>Greetings to React</h1>
+      <Student name="Linkon" age="14"></Student>
     </>
+  )
+}
+
+function Linkon() {
+  const devStyle = {
+    padding: "20px",
+    border: "2px solid BlueViolet",
+    borderRadius: "8px",
+  }
+
+  return devStyle;
+}
+
+function Student(props) {
+  return (
+    <div style={{
+      padding: "20px",
+      border: "2px solid tomato",
+      borderRadius: "8px"
+    }}>
+      <p>My name is {props.name}</p>
+      <p>My age is {props.age}</p>
+    </div>
   )
 }
 
